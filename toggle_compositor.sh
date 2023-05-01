6c32ff16a2ac950a21d96a9cd60003df3f70daff
elkip <mitchiscool51@gmail.com>
2023-04-30 23:41:00 -0400

    Added comment

--- toggle_compositor.sh
+++ toggle_compositor.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# Replace picom with your compositor of choice
+# Useful for gaming as compositors hinder FPS
 
 if [ $(ps -ax|grep picom|grep -v grep|wc -l) -gt '0' ]; then
 	echo "killing picom"
 	killall picom
 else
 	echo "launching picom"
-	nohup picom&
+	nohup picom >/dev/null 2>&1 &
 fi
